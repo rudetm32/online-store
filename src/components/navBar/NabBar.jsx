9240724d0f09767b94d70df486c7c559fb6e18cf
rudetm32 <jaruiz721818@gmail.com>
2023-05-30 23:56:07 -0600

    feat(src/components/navBar/NabBar.jsx):barra navegacion

--- src/components/navBar/NabBar.jsx
+++ src/components/navBar/NabBar.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 
 import styles from './NavBar.module.css';
 
+
 export default function NabBar() {
   return (
-  <div className = { styles.conteiner }>
-    <header>
-        NavBar
-    </header>
-  </div>
+    <nav className={styles.conteiner}>
+      <ul>
+        <li><a href="/">Page 1</a></li>
+        <li><a href="#">Page 2</a></li>
+        <li><a href="#">Page 4</a></li>
+      </ul>
+    </nav>
   )
 }
